34237911e51f7002715029d27f45e91a4ec450e7
Will Larson <lethain@gmail.com>
2008-10-18 18:41:06 -0400

    Cleaned up JavaScript.

--- static/rip.js
+++ static/rip.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
     var selected;
     var reset_selected_skills = function() {
@@ -13,34 +12,22 @@ $(document).ready(function() {
       for (var i=0; i<experiences.length; i++) {
 	var obj = experiences[i];
 	if (title == obj.title) {
-	  for (var j=0; j<obj.skills.length; j++) {
-	    select_skill(obj.skills[j]);
-	  }
+	  for (var j=0; j<obj.skills.length; j++) select_skill(obj.skills[j]);
 	  break;
-	}
-      }
-
-    }
-
+	}}};
     var reset_selected_experience = function() {
-      if (selected) {
-	$(selected).removeClass("selected");
-	$(selected).children("p").hide(); //('slow',function() {});
-	selected = null;
-      }
+      if (selected) $(selected).removeClass("selected").children("p").hide();
     };
     var select_experience = function() {
       reset_selected_experience();
-      selected = this;
-      $(this).addClass("selected");
-      $(this).children("p").slideDown('slow',function() {});
       reset_selected_skills();
+      selected = this;
+      $(this).addClass("selected").children("p").slideDown('slow',function() {});
       select_skills_for_experience(this);
     };
     $(".experience").hover(select_experience, function(){});
- 
     selected = $(".experience:first")[0];
     var first = $(".experience:first").addClass("selected").children("p").slideDown('slow');
     select_skills_for_experience($(".experience:first")[0]);
-    //select_skill("Cappuccino");
   });
+
