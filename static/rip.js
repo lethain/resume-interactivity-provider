91ac266d930809d6e46b5b79b66ef3108748692d
Will Larson <lethain@gmail.com>
2008-10-18 17:56:05 -0400

    Can now view the different experiences.

--- static/rip.js
+++ static/rip.js
@@ -1,12 +1,21 @@
 
 $(document).ready(function() {
+    var selected;
+    var reset_selected_experience = function() {
+      if (selected) {
+	$(selected).removeClass("selected");
+	$(selected).children("p").slideUp('slow',function() {});
+	selected = null;
+      }
+    };
     var select_experience = function() {
+      reset_selected_experience();
+      selected = this;
       $(this).addClass("selected");
+      $(this).children("p").slideDown('slow',function() {});
     };
-    var unselect_experience = function() {
-      //$(this).removeClass("selected");
-    };
-    $(".experience").hover(select_experience, unselect_experience);
+    $(".experience").hover(select_experience, function(){});
     
-
+    var first = $(".experience:first").addClass("selected").children("p").slideDown('slow');
+    window.console.log(first);
   });
