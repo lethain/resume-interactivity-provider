bb8459eb3330a07dfaf05482a681174805d76ed7
Will Larson <lethain@gmail.com>
2008-10-18 16:40:39 -0400

    Much of the raw visuals are done.

--- settings.py
+++ settings.py
@@ -3,43 +3,45 @@ ROOT_PATH = os.path.dirname(__file__)
 
 EXPERIENCES = (
     {
+        'title':'Irrational Exuberance',
+        'link':'http://lethain.com/',
+        'start':'May, 2007',
+        'end':'Present',
+        'skills':('Writing',),
+        'description':'I write about Django, Python and many other things.',
+        },
+    {
         'title':'Sparklines.js',
-        'link':'http://',
-        'start':'',
-        'end':'',
+        'link':'http://lethain.com/',
+        'start':'June 2008',
+        'end':'Present',
+        'description':'A JavaScript library for creating statistically sound cross-browser sparklines.',
         'skills':('JavaScript','Processing.js'),
         },
     {
         'title':'LifeFlow',
-        'link':'http://',
+        'link':'http://lethain.com/',
         'start':'',
         'end':'',
         'skills':('Django','Python','HTML','CSS','JavaScript','jQuery','PostgreSQL','Memcached','Git'),
         },
     {
         'title':'Japanese Exchange and Teaching Program',
-        'link':'http://',
+        'link':'http://lethain.com/',
         'start':'',
         'end':'',
         'skills':('Japanese','Teaching',),
         },
     {
         'title':'Centre College',
-        'link':'http://',
+        'link':'http://lethain.com/',
         'start':'',
         'end':'',
         'skills':('Java',),
         },
     {
-        'title':'Irrational Exuberance',
-        'link':'http://',
-        'start':'',
-        'end':'',
-        'skills':('Writing',),
-        },
-    {
         'title':'Processed Tower Defense',
-        'link':'http://',
+        'link':'http://lethain.com/',
         'start':'',
         'end':'',
         'skills':('JavaScript','Processing.js','HTML','CSS'),
@@ -62,7 +64,7 @@ SKILLS = (
     'Java',
     'PHP',
     'Common Lisp',
-    'Django'
+    'Django',
     'Cocoa',
     'PyObjC',
     'iPhone',
@@ -99,24 +101,42 @@ STATIC_IMAGE_FORMAT = STATIC_URL_FORMAT % u"image/%s"
 THUMBNAIL_SIZE = (128,128)
 
 NAME = u"Will Larson"
+NAME_URL = u"http://lethain.com/"
 EMAIL = u"lethain@gmail.com"
 PHONE = u"(828) 275-9714"
+TWITTER = u"Lethain"
 STREET = u"265 Linden Ave."
 CITY = "Verona"
 ZIP = "07044"
 STATE = "NJ"
 COUNTRY = "USA"
 
+STATEMENT = """
+I am the contract programmer who will solve your problem.
+I won't disappear into another dimension while working; I'll
+keep you informed. I won't conduct maniacal experiments with
+your project; I'll build it with appropriate tools, adhering
+to relevant standards.
+
+I do web development with Django, Python, PHP, JavaScript, HTML
+and CSS. I develop Apple software (OSX and iPhone) with Objective-C,
+Cocoa and PyObjC.
+
+"""
+
 # creating default rendering context
 CONTEXT = {
     'name':NAME,
+    'name_url':NAME_URL,
     'email':EMAIL,
     'phone':PHONE,
+    'twitter':TWITTER,
     'street':STREET,
     'city':CITY,
     'zip':ZIP,
     'state':STATE,
     'country':COUNTRY,
+    'statement':STATEMENT,
     'experiences':EXPERIENCES,
     'skills':SKILLS,
 }
