0dcde4b9cd2b56d9696d42eb07efddf1de451816
Will Larson <lethain@gmail.com>
2008-10-18 18:34:54 -0400

    Now highlight tags correctly.

--- static/rip.js
+++ static/rip.js
@@ -1,10 +1,33 @@
 
 $(document).ready(function() {
     var selected;
+    var reset_selected_skills = function() {
+      $(".skill").removeClass("selected");
+    }
+    var select_skill = function(name) {
+      var id = encodeURIComponent(name);
+      $("#"+id).addClass("selected");
+    }
+    var select_skills_for_experience = function(exp) {
+      var title = decodeURIComponent(exp.id);
+      for (var i=0; i<experiences.length; i++) {
+	var obj = experiences[i];
+	if (title == obj.title) {
+	  var skills = obj.skills;
+	  for (var j=0; j<skills.length; j++) {
+	    var skill = skills[j];
+	    select_skill(skill);
+	  }
+	  break;
+	}
+      }
+
+    }
+
     var reset_selected_experience = function() {
       if (selected) {
 	$(selected).removeClass("selected");
-	$(selected).children("p").slideUp('slow',function() {});
+	$(selected).children("p").hide(); //('slow',function() {});
 	selected = null;
       }
     };
@@ -13,9 +36,12 @@ $(document).ready(function() {
       selected = this;
       $(this).addClass("selected");
       $(this).children("p").slideDown('slow',function() {});
+      reset_selected_skills();
+      select_skills_for_experience(this);
     };
     $(".experience").hover(select_experience, function(){});
     
     var first = $(".experience:first").addClass("selected").children("p").slideDown('slow');
-    window.console.log(first);
+    select_skills_for_experience($(".experience:first")[0]);
+    //select_skill("Cappuccino");
   });
